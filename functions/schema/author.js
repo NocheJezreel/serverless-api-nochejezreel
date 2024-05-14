d4ac84a44147e815acd20befb925c3b931ac154b
NocheJezreel <127300806+NocheJezreel@users.noreply.github.com>
2024-05-14 22:48:55 +0800

    Update author.js

--- functions/schema/author.js
+++ functions/schema/author.js
@@ -1,24 +1,59 @@
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const authorSchema = new Schema({ 
-    name: {
+const authorSchema = new Schema({
+    firmName: {
         type: String,
         required: true,
     },
-    age: {
-        type: String,
+    no: {
+        type: Number,
+        required: true,
+    },
+    amountOfAssistance: {
+        type: Number,
+        required: true,
+    },
+    // Fields for Cumulative section
+    balance: {
+        type: Number,
+        required: true,
+    },
+    totalRefundedAmount: {
+        type: Number,
+        required: true,
+    },
+    totalAmountPayable: {
+        type: Number,
+        required: true,
+    },
+    delayedMonths: {
+        type: Number,
+        required: true,
+    },
+    // Fields for Monthly section
+    due: {
+        type: Number,
+        required: true,
+    },
+    payable: {
+        type: Number,
+        required: true,
+    },
+    arrears: {
+        type: Number,
         required: true,
     },
     username: String,
     password: String,
 });
-authorSchema.pre('save', function (next) {
 
-const username = this.name.toLowerCase().replace(/\s/g, '');
-const password = `${this.name}${this.age}`;
-this.username = username;
-this.password = password; 
-next();
+authorSchema.pre('save', function (next) {
+    const username = this.firmName.toLowerCase().replace(/\s/g, '');
+    const password = `${this.firmName}${this.no}`;
+    this.username = username;
+    this.password = password;
+    next();
 });
+
 module.exports = authorSchema;
